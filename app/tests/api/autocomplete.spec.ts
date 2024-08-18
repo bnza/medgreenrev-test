bb245021fb1d3ad1a8e6a418c355f1bfd6e7ca1f
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2024-08-18 12:57:02 +0200

    feat(core): add only authorized flag in api autocomplete

--- app/tests/api/autocomplete.spec.ts
+++ app/tests/api/autocomplete.spec.ts
@@ -7,17 +7,46 @@ test.beforeAll(() => {
 })
 test('Sites', async ({ request }) => {
   let response = await request.get(`${apiUrl}/autocomplete/sites`)
-  expect(response.json(), 'Return expected entries number').toArrayHaveCount(10)
+  expect(response.json(), 'Returns expected entries number').toArrayHaveCount(
+    10,
+  )
   response = await request.get(`${apiUrl}/autocomplete/sites?search=al`)
-  expect(response.json(), 'Return expected entries number').toArrayHaveCount(6)
+  expect(response.json(), 'Returns expected entries number').toArrayHaveCount(6)
+  response = await request.get(`${apiUrl}/autocomplete/sites?search=ww`)
+  expect(response.json(), 'Hidden WW site should be hidden').toArrayHaveCount(0)
   const baseToken = await getAuthToken()
 
+  response = await request.get(`${apiUrl}/autocomplete/sites`, {
+    headers: {
+      Authorization: baseToken,
+    },
+  })
+  expect(response.json(), 'Returns expected entries number').toArrayHaveCount(
+    10,
+  )
+  response = await request.get(`${apiUrl}/autocomplete/sites?search=al`, {
+    headers: {
+      Authorization: baseToken,
+    },
+  })
+  expect(response.json(), 'Returns expected entries number').toArrayHaveCount(6)
+  response = await request.get(`${apiUrl}/autocomplete/sites?search=ww`, {
+    headers: {
+      Authorization: baseToken,
+    },
+  })
+  expect(response.json(), 'Hidden WW site should be visible').toArrayHaveCount(
+    1,
+  )
   response = await request.get(`${apiUrl}/autocomplete/sites/authorized`, {
     headers: {
       Authorization: baseToken,
     },
   })
-  expect(response.json(), 'Return expected entries number').toArrayHaveCount(2)
+  expect(
+    response.json(),
+    'Authorized sites returns expected entries number',
+  ).toArrayHaveCount(2)
   response = await request.get(
     `${apiUrl}/autocomplete/sites/authorized?search=ed`,
     {
@@ -26,5 +55,8 @@ test('Sites', async ({ request }) => {
       },
     },
   )
-  expect(response.json(), 'Return expected entries number').toArrayHaveCount(1)
+  expect(
+    response.json(),
+    'Authorized sites filtered returns expected entries number',
+  ).toArrayHaveCount(1)
 })
